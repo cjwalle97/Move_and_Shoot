c14707553826ce228b75807c5d65faa19b219236
Christopher Walle <cjwalle97@gmail.com>
2018-01-18 17:07:57 -0600

    Initial Commit
    Networking Assignment1/18/2018

---
+++ Assets/Behaviours/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+public class PlayerController : NetworkBehaviour
+{
+    private float speed = 0.1f;
+
+	void Update ()
+    {
+        if(!isLocalPlayer)
+        {
+            return;
+        }
+        var x = Input.GetAxis("Horizontal") * speed;
+        var z = Input.GetAxis("Vertical") * speed;
+        transform.position += new Vector3(x, 0, z);
+	}
+}
